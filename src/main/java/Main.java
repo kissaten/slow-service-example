5b83b695ce566b6c454e01fda9a4215d2c8f81c9
Joe Kutner <jpkutner@gmail.com>
2015-04-06 22:31:16 -0500

    Updated example to use servlet 3.1 non-blocking

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,48 +14,73 @@ import java.util.concurrent.*;
 
 public class Main extends HttpServlet {
 
-  private ExecutorService executor = Executors.newFixedThreadPool(10);
-
-  private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(10);
-
   @Override
-  protected void doGet(HttpServletRequest req, final HttpServletResponse resp)
+  protected void doGet(HttpServletRequest request, final HttpServletResponse response)
       throws ServletException, IOException {
 
-    FutureTask<String> service =
-        new FutureTask<>(new Callable<String>() {
-          public String call() {
-            return serviceThatIsSlow();
-          }});
-    executor.execute(service);
+    response.addHeader("Transfer-Encoding", "chunked");
+    response.addHeader("Content-Type", "text/html");
 
-    ScheduledFuture chunks = scheduler.scheduleAtFixedRate(new Runnable() {
-        public void run() {
+    final AsyncContext async = request.startAsync();
+    async.setTimeout(90000);
+
+    async.addListener(new AsyncListener() {
+      @Override
+      public void onComplete(AsyncEvent event) throws IOException {
+        System.out.println("Async complete");
+      }
+
+      @Override
+      public void onError(AsyncEvent event) {
+        System.out.println("Async error");
+        System.out.println(event.getThrowable());
+      }
+
+      @Override
+      public void onStartAsync(AsyncEvent event) {
+        System.out.println("Async starting");
+      }
+
+      @Override
+      public void onTimeout(AsyncEvent event) {
+        System.out.println("Async timeout");
+      }
+    });
+
+    final ScheduledFuture chunkBlower = Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Runnable() {
+      public void run() {
+        try {
+          async.getResponse().getWriter().print(" ");
+          async.getResponse().getWriter().flush();
+        } catch (IOException e) {
+          // do nothing
+        }
+      }
+    }, 15, 15, TimeUnit.SECONDS);
+
+    Executors.newSingleThreadExecutor().execute(new Runnable() {
+      public void run() {
+        String responseString = "";
+        try {
+          responseString = serviceThatIsSlow();
+        } catch (Exception e) {
+          responseString = "<p>There was an error calling the service!</p>";
+        } finally {
+          chunkBlower.cancel(true);
           try {
-            resp.getWriter().print(" ");
-            resp.getWriter().flush();
+            async.getResponse().getWriter().print(responseString);
+            async.getResponse().getWriter().flush();
           } catch (IOException e) {
             // do nothing
           }
+          async.complete();
         }
-      }, 10, 10, TimeUnit.SECONDS);
-
-    resp.addHeader("Transfer-Encoding", "chunked");
-    resp.addHeader("Content-Type", "text/html");
-
-    String responseString = "";
-    try {
-      responseString = service.get();
-    } catch (Exception e) {
-      responseString = "<p>There was an error calling the service!</p>";
-    } finally {
-      chunks.cancel(true);
-      resp.getWriter().print(responseString);
-    }
+      }
+    });
   }
 
   protected String serviceThatIsSlow() {
-    try { Thread.sleep(50000); } catch (InterruptedException e) {}
+    try { Thread.sleep(50000); } catch (InterruptedException e) { /* do nothing */ }
     return "<html><body><p>This is an important message</p></body></html>";
   }
 
